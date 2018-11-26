1b50604f0aa4abebc4fc17faf545e150da501eff
qiaoqiao10001 <qiaoqiao10001@gmail.com>
2018-11-26 17:10:13 +0800

    express 做后台服务

---
+++ routes/index.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.post('/register', function (req, res, next) {
+    const {username, password} = req.body
+    console.log('register', username, password)
+    if (username === 'admin') {
+        res.send({code: 1, msg: ' 此用户已存在'})
+    } else {
+        res.send({code: 0, data: {_id: 'abc', username, password}})
+    }
+})
+
+module.exports = router;
